662ba48ff871702c65804abaec022057708db29e
Purva Rajyaguru <rajyagurupurva28@gmail.com>
2022-08-29 17:18:32 +0530

    added textarea for  user input

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var translatebtn = document.querySelector("#translate");
+var input = document.querySelector("#textin");
 console.log(translatebtn);
+console.log(textin);
 
 translatebtn.addEventListener("click" , function (){
 
        console.log("clicked");
+       console.log("input", textin.value);
+       
 
 
        
