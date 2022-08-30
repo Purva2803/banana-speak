908e3700a1ce01296ced9f992c42c74d33003a49
Purva Rajyaguru <rajyagurupurva28@gmail.com>
2022-08-30 23:31:37 +0530

    added event handler and url

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 var translatebtn = document.querySelector("#translate");
 var input = document.querySelector("#textin");
-console.log(translatebtn);
-console.log(textin);
+var output = document.querySelector("#textout");
+var Url = "https://api.funtranslations.com/translate/minion.json";
 
-translatebtn.addEventListener("click" , function (){
 
-       console.log("clicked");
-       console.log("input", textin.value);
+function getTranslation(text){
+       return Url  + "?" +"text=" + text 
+}
+
+function errorHandler(error){
+       console.log("error occured",error);
+       alert("there is something wrong try again");
+}
+
+function EventHandler(){
+
+       var inputtext = input.value;
+   
+       fetch (getTranslation(inputtext))
        
-
-
+       .then(response=>response.json())
+       .then(reply=> {
+              var translatedtext = reply.contents.translated;
+              output.innerText = translatedtext;
+       }
+       );
        
-})
+}
+
+
+translatebtn.addEventListener("click",EventHandler);
